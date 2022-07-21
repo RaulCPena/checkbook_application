c133268378622ea0c4367d9ce9e056db1ed57f26
Raul Pena <raul.cpena@gmail.com>
2022-07-21 11:02:36 -0500

    updated to pep8

--- checkbook.py
+++ checkbook.py
@@ -4,7 +4,7 @@ print("💰💰💰 Welcome to your bank terminal! 💰💰💰~")
 time.sleep(.4)
 print("\n")
 
-options =(
+options = (
     "What would you like to do? \n\n"
     "1) view your current balance\n"
     "2) record a debit (withdrawal)\n"
@@ -23,10 +23,10 @@ while True:
         with open("check_book.txt", "r") as f:
             fl = f.readlines()
             for line in fl:
-                bwalance = balance + float(line)
+                balance = balance + float(line)
             print(f"Your balance is : ${round(balance, 2)}")
             choice = input(options)
-    
+
     if choice == "2":
         with open("check_book.txt", "a") as f:
             debit_amount = input("How much do you want to debit? ")
@@ -35,7 +35,7 @@ while True:
             f.write(debit_amount + "\n")
             print(f"Your total debit is ${debit_amount}")
             choice = input(options)
-    
+
     if choice == "3":
         with open("check_book.txt", "a") as f:
             credit_amount = input("How mucbh do you want to credit? ")
@@ -43,16 +43,14 @@ while True:
             credit_amount = float(credit_amount)
             print(f"Your total credit is ${credit_amount}")
             choice = input(options)
-    
+
     if choice == "4":
         with open("check_book.txt") as f:
             file_contents = f.read()
             print(file_contents)
             choice = input(options)
-              
-    
+
     if choice == "5":
         print("Have a great day 👋👋")
         time.sleep(.4)
         break
-           
